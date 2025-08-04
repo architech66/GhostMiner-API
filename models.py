e50d50ef92b076908ae6cbf24c62cacba65d318c
architech66 <104875975+architech66@users.noreply.github.com>
2025-08-04 11:34:19 -0700

    models.py

--- models.py
+++ models.py
@@ -7,10 +7,10 @@ import datetime
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,   primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    id            = db.Column(db.Integer, primary_key=True)
+    username      = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    created_at   = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    created_at    = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def set_password(self, pw):
         self.password_hash = generate_password_hash(pw)
