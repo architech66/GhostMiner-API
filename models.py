36a4c93dfd0fb8f5ce8190d541ac5b86acc7c0a0
architech66 <104875975+architech66@users.noreply.github.com>
2025-08-04 11:28:59 -0700

    Create models.py

---
+++ models.py
@@ -0,0 +1,31 @@
+# models.py
+
+from extensions import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,   primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at   = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def set_password(self, pw):
+        self.password_hash = generate_password_hash(pw)
+
+    def check_password(self, pw):
+        return check_password_hash(self.password_hash, pw)
+
+class LoginLog(db.Model):
+    __tablename__ = 'login_logs'
+    id         = db.Column(db.Integer, primary_key=True)
+    username   = db.Column(db.String(80))
+    ip_address = db.Column(db.String(45))
+    successful = db.Column(db.Boolean, default=False)
+    timestamp  = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
